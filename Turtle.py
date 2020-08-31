3eddaadf30b428f007711b709b0c034476aa7d76
Aman Amrit <amanamritcse@gmail.com>
2020-08-31 12:16:15 +0530

    Some changes

---
+++ Turtle.py
@@ -0,0 +1,11 @@
+#using linter as flake8 for GUI
+import turtle
+abc_turtle = turtle.Turtle()
+abc_turtle.forward(200)
+abc_turtle.right(90)
+abc_turtle.forward(200)
+abc_turtle.right(90)
+abc_turtle.forward(200)
+abc_turtle.right(90)
+abc_turtle.forward(200)
+input("Enter any key to close.....")
